6ca74350decc77a97c766774032a8cf7370df48c
rkumar430@rku.ac.in <raghuwinderkumar24k@gmail.com>
2026-01-02 08:10:47 +0530

    commit

--- class_work/src/App.jsx
+++ class_work/src/App.jsx
@@ -1,15 +1,18 @@
 import PropsExample from "./working on/props_example";
 import React from "react";
 import DefaultProps from "./working on/defaultprops";
+import Form_Exercise from "./working on/form_exercise";
 function App() {
     return (
         <>
-            <PropsExample
+            {/* <PropsExample
                 image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTALbFf1X2wRJT6zHjrktK_Mt9yb0jnBLZb7w&s"
                 button_txt="View Profile"
                 date="2024-12-26"
-            />
+            /> */}
             {/* <DefaultProps /> */}
+            <Form_Exercise />
+            
         </>
     )
 }
--- class_work/src/working on/form_exercise.jsx
+++ class_work/src/working on/form_exercise.jsx
@@ -1,8 +1,25 @@
-// import { Component } from "react";
-import React, {Component}  from "react";
+import React, { Component } from "react";
+import { TextField, Box } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
 
-class FormExercise extends Component{
-    render(){
-
+class FormExercise extends Component {
+    render() {
+        return (
+            <Box sx={{ padding: 2 }}>
+                <HomeIcon sx={{ fontSize: 40, marginBottom: 2 }} />
+                <TextField
+                    helperText="Please enter your name"
+                    id="demo-helper-text-misaligned"
+                    label="Name"
+                    sx={{ marginRight: 2 }}
+                />
+                <TextField
+                    id="demo-helper-text-misaligned-no-helper"
+                    label="Name"
+                />
+            </Box>
+        )
     }
 }
+
+export default FormExercise;
