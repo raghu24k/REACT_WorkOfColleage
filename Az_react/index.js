d2c02099cca7887488ae544ba0e4e0482b33fa2b
rkumar430@rku.ac.in <raghuwinderkumar24k@gmail.com>
2026-01-05 11:26:37 +0530

    okk

--- Az_react/index.js
+++ Az_react/index.js
@@ -18,3 +18,7 @@ function ArthimeticOperation(a, b){
 ArthimeticOperation(11,22);
 
 // create a functionn whcih  will return *3 if odd else *2 if num is even
+const myname = (n) => n % 2 == 0 ? 2*n: 3*n;
+console.log(myname(5));
+console.log(myname(4));
+console.log(myname(11));
