5d468fe5faad3fac9b51aee02e71a0fa2df68447
rkumar430@rku.ac.in <raghuwinderkumar24k@gmail.com>
2026-01-05 11:16:18 +0530

    function

---
+++ Az_react/index.js
@@ -0,0 +1,18 @@
+console.log("hi,raghu");
+
+
+
+function myfunction(fname, lname){
+    // console.log("welcome " + fname + " " + lname);
+    console.log(`welcome ${fname} ${lname}`);
+}
+myfunction('Raghuwinder', 'Kumar');
+
+
+function ArthimeticOperation(a, b){
+    console.log(`the addition of ${a} and ${b} is ${a + b}`);
+    console.log(`the subtraction of ${a} and ${b} is ${a - b}`);
+    console.log(`the multiplication of ${a} and ${b} is ${a * b}`);
+    console.log(`the division of ${a} and ${b} is ${a / b}`);
+}
+ArthimeticOperation(11,22);
