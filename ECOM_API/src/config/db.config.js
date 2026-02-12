69259018a2e87034eba52297f3cedcc4406c9283
rkumar430@rku.ac.in <raghuwinderkumar24k@gmail.com>
2026-02-12 11:18:50 +0530

    building ecom

---
+++ ECOM_API/src/config/db.config.js
@@ -0,0 +1,11 @@
+const MYSQL_DB_CONFIG = {
+    HOST: "localhost",
+    USER: "root",  //this is the super Admin into the database because it's a only one User of the database
+    PORT: 3000,
+    PASSWORD: "",
+    DB: "my_ecom_db",
+  };
+  module.exports = {
+    MYSQL_DB_CONFIG,
+  };
+  
